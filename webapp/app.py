603e9b41aef0da5390b256fecbb5c0d2b3ad028e
NWalker4483 <nilezwalker@gmail.com>
2026-08-03 07:07:13 -0400

    feat: Enable mobile-web-app-capable and apple-mobile-web-app-capable meta tags

--- webapp/app.py
+++ webapp/app.py
@@ -342,6 +342,8 @@ INDEX_HTML = r"""<!DOCTYPE html>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
 <title>Screenshot → Anki</title>
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin" />
